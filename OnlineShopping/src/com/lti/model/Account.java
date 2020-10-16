5f0abebc4b947311bb5a1038e551607f63db5413
ShivamPali-2020 <pali.shivam2020@gmail.com>
2020-10-16 09:50:31 +0530

    Added Account class through Branch 1

---
+++ OnlineShopping/src/com/lti/model/Account.java
@@ -0,0 +1,20 @@
+package com.lti.model;
+
+public class Account {
+	int acccoundId;
+	double balance;
+	public int getAcccoundId() {
+		return acccoundId;
+	}
+	public void setAcccoundId(int acccoundId) {
+		this.acccoundId = acccoundId;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	
+}
